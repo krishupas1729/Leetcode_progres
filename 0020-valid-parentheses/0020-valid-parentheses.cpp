7782a85e6e7aec08990675b0f356753c02f4dad3
Krishu_1729 <krishupas1729@gmail.com>
2026-07-19 07:45:37 +0530

    Time: 0 ms (100%), Space: 8.8 MB (68.05%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(size_t i=0; i < s.length(); i++) {
+            if(st.empty()) {
+                st.push(s[i]);
+            }
+            else if(st.top() == '(' && s[i] == ')' || st.top() == '[' && s[i] == ']' || st.top() == '{' && s[i] == '}' ) {
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        
+        return st.empty();
+    }
+};
